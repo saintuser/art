4affa2001501dfac772d86e74dae1511352260f0
kristjanjansen <kristjan.jansen@gmail.com>
2021-02-08 11:03:02 +0200

    cleanup

--- src/components/VideoHsl.vue
+++ src/components/VideoHsl.vue
@@ -1,15 +1,22 @@
 <template>
-  <button @click="muted = !muted">{{ muted ? "unmute" : "mute" }}</button>
-  <!-- <div>{{ isLoading ? "loading" : "playing" }}</div> -->
-  {{ status }}
-  <div class="debug aspect">
-    <video
-      controls
-      ref="videoRef"
-      autoplay
-      :muted="muted"
-      style="width: 100%; aspect-ratio: 1 / 1"
-    />
+  <div>
+    <button @click="muted = !muted">{{ muted ? "unmute" : "mute" }}</button>
+    <!-- <div>{{ isLoading ? "loading" : "playing" }}</div> -->
+    {{ status }}
+    <div>{{ width }} {{ height }}</div>
+    <div class="debug aspect">
+      <video
+        controls
+        ref="videoRef"
+        autoplay
+        :muted="muted"
+        :style="{
+          display: 'block',
+          width: '100%',
+          aspectRatio: width + ' / ' + height,
+        }"
+      />
+    </div>
   </div>
 </template>
 
@@ -18,6 +25,6 @@ import { defineProps, ref } from "vue";
 import { useHls } from "../lib";
 
 const props = defineProps({ src: String });
-const { videoRef, isLoading, status } = useHls(props.src);
+const { videoRef, status, width, height } = useHls(props.src);
 const muted = ref(true);
 </script>
--- src/lib/index.js
+++ src/lib/index.js
@@ -12,47 +12,37 @@ export function debounce(fn, timeout) {
 const hlsReloadFrequency = 1000;
 
 export const useHls = (src) => {
-  const retryDelay = 4000;
+  const retryDelay = 3000;
+
   const videoRef = ref(null);
-  const isLoading = ref(true);
+
   const status = ref("nodata");
+  const width = ref(null);
+  const height = ref(null);
 
   onMounted(() => {
-    videoRef.value.addEventListener("play", (e) => {
-      console.log("play");
-    });
-    videoRef.value.addEventListener("playing", (e) => {
-      console.log("playing");
-      isLoading.value = false;
-      status.value = "playing";
-    });
-    videoRef.value.addEventListener("ended", (e) => {
-      console.log("ended");
-    });
     videoRef.value.addEventListener("loadedmetadata", (e, e2) => {
       console.log("loadedmetadata");
       if (videoRef.value) {
-        console.log(videoRef.value.videoWidth, videoRef.value.videoHeight);
+        width.value = videoRef.value.videoWidth;
+        height.value = videoRef.value.videoHeight;
       }
     });
-    videoRef.value.addEventListener("ended", (e) => {
-      console.log("ended");
-      status.value = "nodata";
+
+    videoRef.value.addEventListener("loadeddata", (e) => {
+      status.value = "loading";
     });
+
+    videoRef.value.addEventListener("playing", (e) => {
+      status.value = "playing";
+    });
+
     videoRef.value.addEventListener("emptied", (e) => {
-      console.log("emptied");
       status.value = "nodata";
     });
-    videoRef.value.addEventListener("stalled", (e) => {
-      console.log("stalled");
-      //   isLoading.value = false;
-      //   status.value = "loading";
-    });
-    videoRef.value.addEventListener("suspend", (e) => {
-      console.log("suspend");
-    });
-    videoRef.value.addEventListener("waiting", (e) => {
-      console.log("waiting");
+
+    videoRef.value.addEventListener("ended", (e) => {
+      status.value = "nodata";
     });
 
     if (videoRef.value.canPlayType("application/vnd.apple.mpegURL")) {
@@ -67,50 +57,53 @@ export const useHls = (src) => {
       }, 1000);
 
       videoRef.value.addEventListener("loadeddata", (e) => {
-        console.log("loadeddata, clearing timeout");
-        status.value = "loading";
-        clearTimeout(timeout);
+        console.log("clearing timeout");
+        if (timeout) {
+          clearTimeout(timeout);
+        }
       });
+
       videoRef.value.addEventListener("waiting", (e) => {
-        isLoading.value = true;
+        status.value = "loading";
         timeout = setInterval(() => {
           console.log("waiting interval");
           videoRef.value.src = src;
-        }, 1000);
-        //videoRef.value.src = src;
-        // debounce(() => {
-        //   videoRef.value.src = src;
-        //   console.log("debounce");
-        // }, hlsReloadFrequency);
+        }, hlsReloadFrequency);
       });
-    } /*else if (Hls.isSupported()) {
-      console.log("CHROME");
-      const hls = new Hls({
-        manifestLoadingRetryDelay: hlsReloadFrequency,
-        manifestLoadingMaxRetry: Infinity,
-        xhrSetup: function (xhr, url) {
-          xhr.addEventListener("error", (e) => {
-            console.log("xhr error");
-            videoRef.value.play();
-          });
-        },
-      });
+    } else {
+      if (Hls.isSupported()) {
+        console.log("CHROME");
+        const hls = new Hls({
+          manifestLoadingRetryDelay: hlsReloadFrequency,
+          manifestLoadingMaxRetry: Infinity,
+          xhrSetup: function (xhr) {
+            xhr.addEventListener("error", (e) => {
+              console.log("xhr error");
+              hls.loadSource(src);
+              hls.startLoad();
+              videoRef.value.play();
+            });
+          },
+        });
 
-      hls.attachMedia(videoRef.value);
-      hls.on(Hls.Events.MEDIA_ATTACHED, () => {
-        hls.loadSource(src);
-        hls.startLoad();
-      });
-      hls.on(Hls.Events.ERROR, function (e, data) {
-        console.log("hlserror");
-        console.log(data);
-        if (data.type !== Hls.ErrorTypes.MEDIA_ERROR) {
+        hls.attachMedia(videoRef.value);
+        hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+          hls.loadSource(src);
           hls.startLoad();
-        } else {
+        });
+        hls.on(Hls.Events.ERROR, function (e, data) {
+          console.log("hlserror");
+          console.log(data);
           hls.recoverMediaError();
-        }
-      });
-    }*/
+          if (data.type !== Hls.ErrorTypes.MEDIA_ERROR) {
+            hls.startLoad();
+          } else {
+            hls.recoverMediaError();
+          }
+        });
+      }
+    }
   });
-  return { videoRef, isLoading, status };
+
+  return { videoRef, status, width, height };
 };
