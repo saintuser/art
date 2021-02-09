b7ed492678172239a0d4ed6abbd2ed08c47c180e
kristjanjansen <kristjan.jansen@gmail.com>
2021-02-09 19:26:09 +0200

    ical

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,4 +6,15 @@
 <script setup>
 import { useEvents } from "../lib/index.js";
 const events = useEvents();
+
+import { ICalendar } from "datebook";
+
+const e = ["a", "b"];
+const formatEvent = (e) => ({ title: e, start: new Date(), end: new Date() });
+
+const calendar = new ICalendar(formatEvent(e[0]));
+e.slice(1).forEach((e) => calendar.addEvent(new ICalendar(formatEvent(e))));
+
+const cal = calendar.render();
+console.log(cal);
 </script>
