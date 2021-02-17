118de45b818ed17c548dd0ec2074052c3562f3c3
kristjanjansen <kristjan.jansen@gmail.com>
2021-02-17 21:34:17 +0200

    rolling back the fetch

--- src/lib/sheet.js
+++ src/lib/sheet.js
@@ -1,5 +1,3 @@
-import { replace, config } from ".";
-
 //@ts-check
 const parseSheet = (data) => {
   const title = data.feed.title.$t;
@@ -21,12 +19,9 @@ const parseSheet = (data) => {
 
 export const fetchSheet = (url) => {
   const id = url.match(/[-\w]{25,}/)?.[0];
-  const sheetUrl = `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`;
-  const fetchUrl = replace(config.corsUrl, {
-    url: sheetUrl,
-    random: Math.random(),
-  });
-  return fetch(fetchUrl)
+  return fetch(
+    `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
+  )
     .then((res) => res.json())
     .then((res) => parseSheet(res));
 };
